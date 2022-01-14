7677f40556467a53623429132fbff23da99d2659
sofiaversoza <sofversoza@gmail.com>
2022-01-14 13:19:01 -0700

    commit tree c9615caff4448ed60da750e1efaa3046490fdb98

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    def name=(value)    # setter
+        @name = value
+    end
 
+    def name            # getter for name
+        @name
+    end
+
+    def breed=(value)   # setter for breed
+        @breed = value
+    end
+
+    def breed           # getter for breed
+        @breed
+    end
 end
+
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
 
+    def name=(value)    # setter
+        @name = value
+    end
+
+    def name
+        @name 
+    end
+
+    def job=(value)    # setter
+        @job = value
+    end
+
+    def job
+        @job
+    end
 end
